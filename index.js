17803f40cbf7f129520a83080b47e62973b867d7
egentinnamdi <egentinnamdi10@gmail.com>
2026-04-28 20:34:37 +0100

    refactor: export Express app and disable server startup
    Make the app exportable for use in testing or as a module, and comment out the server startup code to allow external control.

--- index.js
+++ index.js
@@ -356,6 +356,8 @@ app.get("/", (req, res) => {
   );
 });
 
+export default app;
+
 // === START SERVER ===
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
